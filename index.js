ca320e5e26f45cd435a85b81ca798d1f4576c355
Shade78 <shademoon322@gmail.com>
2024-08-22 23:10:27 +0300

    Init
    register bot and initialise project (with grammy, dotenv and nodemon)

--- index.js
+++ index.js
@@ -3,4 +3,14 @@ const { Bot } = require("grammy");
 
 const bot = new Bot(process.env.BOT_API_KEY);
 
+// команда "/start"
+bot.command("start", async (ctx) => {
+  await ctx.reply("test first message from bot");
+});
+
+// ответ на любое другое сообщение
+bot.on("message", async (ctx) => {
+  await ctx.reply("got another message");
+});
+
 bot.start();
