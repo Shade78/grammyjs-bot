626e995628a47013385da3cc4f3413fc623b6fe6
Shade78 <shademoon322@gmail.com>
2024-08-23 23:06:58 +0300

    error handling

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 require("dotenv").config(); // получаем доступ к переменной окружения
-const { Bot } = require("grammy");
+const { Bot, GrammyError, HttpError } = require("grammy");
 
 const bot = new Bot(process.env.BOT_API_KEY);
 
@@ -11,6 +11,21 @@ bot.command("start", async (ctx) => {
 // ответ на любое другое сообщение
 bot.on("message", async (ctx) => {
   await ctx.reply("got another message");
+});
+
+// обработчик ошибок
+bot.catch((err) => {
+  const ctx = err.ctx;
+  console.log(`Error while handling update ${ctx.update.update_id}:`);
+  const e = err.error;
+
+  if (e instanceof GrammyError) {
+    console.error("Error in request:", e.description);
+  } else if (e instanceof HttpError) {
+    console.error("Could not contact Telegram:", e);
+  } else {
+    console.error("Unknows error:", e);
+  }
 });
 
 bot.start();
