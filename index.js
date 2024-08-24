2e7b1a732988c6a20e385a6c52b6a0038bd622e6
Shade78 <shademoon322@gmail.com>
2024-08-24 23:50:35 +0300

    added keyboard and other replies

--- index.js
+++ index.js
@@ -1,19 +1,76 @@
 require("dotenv").config(); // получаем доступ к переменной окружения
-const { Bot, GrammyError, HttpError } = require("grammy");
+const { Bot, GrammyError, HttpError, Keyboard } = require("grammy");
 
 const bot = new Bot(process.env.BOT_API_KEY);
 
+bot.api.setMyCommands([
+  {
+    command: "start",
+    description: "Запуск бота",
+  },
+  {
+    command: "get_user",
+    description: "get data of user from jsonplaceholder",
+  },
+]);
+
 // команда "/start"
 bot.command("start", async (ctx) => {
-  await ctx.reply("test first message from bot");
+  await ctx.reply("logging info from ctx [link](www.youtube.com)", {
+    parse_mode: "MarkdownV2",
+    disable_web_page_preview: true,
+  });
+  await ctx.react("🔥");
 });
 
-// ответ на любое другое сообщение
-bot.on("message", async (ctx) => {
-  await ctx.reply("got another message");
+bot.command("mood", async (ctx) => {
+  const moodKeyboard = new Keyboard()
+    .text("good")
+    .row()
+    .text("normal")
+    .row()
+    .text("bad")
+    .resized();
+  await ctx.reply("How do you feel?", {
+    reply_markup: moodKeyboard,
+  });
 });
 
-// обработчик ошибок
+bot.hears("good", async (ctx) => {
+  await ctx.reply("ok", {
+    reply_markup: { remove_keyboard: true },
+  });
+});
+
+// можно добавить регулярку (bot.hears)
+bot.command(["ID"], async (ctx) => {
+  await ctx.reply(
+    `${ctx.from.first_name} id is:  <span class="tg-spoiler">${ctx.from.id}</span>`,
+    {
+      reply_parameters: { message_id: ctx.msg.message_id },
+      parse_mode: "HTML",
+    }
+  );
+});
+
+bot.command(["say_something", "get_user"], async (ctx) => {
+  const replyMessage = await fetch(
+    "https://jsonplaceholder.typicode.com/users/2"
+  )
+    .then((res) => res.json())
+    .then((json) => json.name + json.email + " " + json.address.street);
+
+  await ctx.reply(`<i>${replyMessage}</i>`, {
+    parse_mode: "HTML",
+  });
+});
+
+// ответ на любое другое сообщение (можно указать тип сообщения)
+bot.on("::url", async (ctx) => {
+  await ctx.reply("got another link");
+});
+
+// обработчик ошибок (из документации)
 bot.catch((err) => {
   const ctx = err.ctx;
   console.log(`Error while handling update ${ctx.update.update_id}:`);
